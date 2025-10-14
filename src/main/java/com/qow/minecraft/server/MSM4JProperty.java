a4b16b6626ee1c164c0c9e02fa9cd471cd711e5b
cooh <qow4programnormal01@gmail.com>
2025-10-14 10:33:47 +0900

    Propertyクラスのイメージはだいたい完成 MSM4JPropertyクラスに登録してMSM4Jに実装したら完了

--- src/main/java/com/qow/minecraft/server/MSM4JProperty.java
+++ src/main/java/com/qow/minecraft/server/MSM4JProperty.java
@@ -9,7 +9,48 @@ import java.io.IOException;
 
 public class MSM4JProperty extends Property {
     public MSM4JProperty(File qonFile) throws UntrustedQONException, IOException {
+        this();
         QONObject qonObject = new QONObject(qonFile);
 
+    }
+
+    public MSM4JProperty() {
+        super();
+
+        addTargetKey("edition");
+        addTargetKey("home-dir");
+        addTargetKey("server-path");
+        addTargetKey("log_loggable");
+        addTargetKey("log_title");
+        addTargetKey("log_extension");
+        addTargetKey("log_time-format");
+        addTargetKey("log_dir");
+        addTargetKey("backup_backupable");
+        addTargetKey("backup_delay");
+        addTargetKey("backup_comment");
+        addTargetKey("backup_title");
+        addTargetKey("backup_extension");
+        addTargetKey("backup_time-format");
+        addTargetKey("backup_backup-dir");
+        addTargetKey("backup_backup-files-path");
+        addTargetKey("jvm-args_before");
+        addTargetKey("jvm-args_after");
+        addTargetKey("notification_webhook-url");
+        addTargetKey("notification_server-wave");
+        addTargetKey("notification_server-status");
+        addTargetKey("notification_log-in-out");
+        addTargetKey("notification_log-in-out-index");
+        addTargetKey("notification_time-format");
+        addTargetKey("control_controllable");
+        addTargetKey("control_bind-ip");
+        addTargetKey("control_server-ip");
+        addTargetKey("control_client-ip");
+        addTargetKey("control_port");
+        addTargetKey("control_byte-size");
+        addTargetKey("control_protocol-id");
+    }
+
+    public void setEdition(String edition) {
+        putMap("edition", edition);
     }
 }
--- src/main/java/com/qow/util/Property.java
+++ src/main/java/com/qow/util/Property.java
@@ -1,42 +1,38 @@
 package com.qow.util;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Property {
     private final List<String> targetKeyList;
+    private final Map<String, String> targetMap;
     private boolean parsed;
-    private Map<Boolean, String> booleanMap;
-    private Map<Byte, String> byteMap;
-    private Map<Character, String> characterMap;
-    private Map<Short, String> shortMap;
-    private Map<Integer, String> integerMap;
-    private Map<Float, String> floatMap;
-    private Map<Long, String> longMap;
-    private Map<Double, String> doubleMap;
-    private Map<String, String> stringMap;
 
     public Property() {
         parsed = false;
         targetKeyList = new ArrayList<>();
+        targetMap = new HashMap<>();
     }
 
     public boolean parse() {
-        boolean[] targetExist = new boolean[targetKeyList.size()];
-        for (int i = 0; i < targetKeyList.size(); i++) {
-            String target = targetKeyList.get(i);
-            if (booleanMap.containsKey(target) || byteMap.containsKey(target) || characterMap.containsKey(target) || shortMap.containsKey(target) || integerMap.containsKey(target) || floatMap.containsKey(target) || longMap.containsKey(target) || doubleMap.containsKey(target) || stringMap.containsKey(target)) {
-                targetExist[i] = true;
+        for (String target : targetKeyList) {
+            if (!targetMap.containsKey(target)) {
+                return false;
             }
-        }
-
-        for (int i = 0; i < targetExist.length; i++) {
-            if (!targetExist[i]) return false;
         }
 
         parsed = true;
         return true;
+    }
+
+    public final void putMap(String key, String value) {
+        targetMap.put(key, value);
+    }
+
+    public final void removeMap(String key) {
+        targetMap.remove(key);
     }
 
     public final void addTargetKey(String key) {
